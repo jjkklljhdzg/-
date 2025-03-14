63f8b5b39ae2760149af91d3276b23a34e92ce4b
jjkklljhdzg <sidorovasona00@gmail.com>
2025-03-15 01:21:16 +0400

    Add files via upload

---
+++ scripts/script2.js
@@ -0,0 +1,37 @@
+function readMore1() {
+    const dots = document.getElementById("dots1");
+    const moreText = document.getElementById("more1");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        moreText.style.display = "inline";
+    }
+}
+
+function readMore2() {
+    const dots = document.getElementById("dots2");
+    const moreText = document.getElementById("more2");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        moreText.style.display = "inline";
+    }
+}
+function readMore3() {
+    const dots = document.getElementById("dots3");
+    const moreText = document.getElementById("more3");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        moreText.style.display = "inline";
+    }
+}
